656baade132c6f9a8e62779d2eedbe6d8c431960
B Phan <bphan@Binh.local>
2023-10-29 17:28:33 -0600

    fixed image slider

--- src/app/src/main/java/com/example/sweethome/EditItemActivity.java
+++ src/app/src/main/java/com/example/sweethome/EditItemActivity.java
@@ -2,12 +2,20 @@ package com.example.sweethome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.smarteist.autoimageslider.SliderView;
 import java.util.ArrayList;
 
 public class EditItemActivity extends AppCompatActivity {
+    FirebaseStorage imageStorage = FirebaseStorage.getInstance();
+    StorageReference imageStorageRef = imageStorage.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,21 +24,32 @@ public class EditItemActivity extends AppCompatActivity {
         // array list for storing our image urls.
         ArrayList<ImageSliderData> sliderDataArrayList = new ArrayList<>();
 
-        // initializing the slider view.
+        // populate the images array list
+        Intent intent = getIntent();
+        if (intent != null) {
+            ArrayList<String> photos = intent.getStringArrayListExtra("photos");
+            for (int i = 0; i < photos.size(); i++) {
+                sliderDataArrayList.add(new ImageSliderData(photos.get(i)));
+            }
+        }
+
+        // get the slider view frame and the slider view.
+        FrameLayout sliderViewFrame = findViewById(R.id.image_slider_frame);
         SliderView sliderView = findViewById(R.id.image_slider);
 
-        // in case the item has no associated images.
-        int noImagePlaceholderSrc = R.drawable.no_image_placeholder;
-        ImageSliderData noImagePlaceholder = new ImageSliderData(noImagePlaceholderSrc);
-        sliderDataArrayList.add(noImagePlaceholder);
+        // get the no image placeholder view.
+        RelativeLayout noImagePlaceholder = findViewById(R.id.no_image_placeholder);
 
-        // add the image urls inside array list
-        // sliderDataArrayList.add(new ImageSliderData(example image url));
+        // Check if there are any images associated with the item.
+        if (!sliderDataArrayList.isEmpty()) {
+            ImageSliderAdapter adapter = new ImageSliderAdapter(this, sliderDataArrayList);
+            sliderView.setSliderAdapter(adapter);
+            noImagePlaceholder.setVisibility(View.GONE);
+            sliderViewFrame.setVisibility(View.VISIBLE);
 
-        // passing this array list inside our adapter class.
-        ImageSliderAdapter adapter = new ImageSliderAdapter(this, sliderDataArrayList);
-
-        // setadapter to sliderview.
-        sliderView.setSliderAdapter(adapter);
+        } else {
+            sliderViewFrame.setVisibility(View.GONE);
+            noImagePlaceholder.setVisibility(View.VISIBLE);
+        }
     }
 }
--- src/app/src/main/java/com/example/sweethome/MainActivity.java
+++ src/app/src/main/java/com/example/sweethome/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Button to open Edit Item screen
+        Button editItemButton = findViewById(R.id.editItemButton);
+        editItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
