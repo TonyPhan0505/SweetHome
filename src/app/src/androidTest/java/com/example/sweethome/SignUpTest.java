7344b97ef185c4ccc9c86579ed564a411d96396e
Beylul Ariaya-Dirrar <beylul@ualberta.ca>
2023-11-28 18:36:38 -0700

    finished sign up tests

--- src/app/src/androidTest/java/com/example/sweethome/SignUpTest.java
+++ src/app/src/androidTest/java/com/example/sweethome/SignUpTest.java
@@ -9,6 +9,7 @@ import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.content.ComponentName;
 
@@ -17,6 +18,12 @@ import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.junit.After;
 import org.junit.Before;
@@ -28,6 +35,10 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class SignUpTest {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference usersRef = db.collection("users");
+    private FirebaseAuth userAuth = FirebaseAuth.getInstance();
+
     @Rule
     public ActivityScenarioRule<SignUpActivity> scenario=new ActivityScenarioRule<SignUpActivity>(SignUpActivity.class);
 
@@ -246,6 +257,49 @@ public class SignUpTest {
         Thread.sleep(3000);
         /* check that we get the expected error (invalid email) */
         onView(withId(R.id.editTextEmailSignUp)).check(matches(hasErrorText("Invalid email address.")));
+    }
+
+    @Test
+    public void testSignUpGoToMain() throws InterruptedException {
+        /* click the email field edit text and put in an email*/
+        onView(withId(R.id.editTextEmailSignUp)).perform(click());
+        onView(withId(R.id.editTextEmailSignUp)).perform(ViewActions.typeText("signuptest@signuptest.ca"));
+        /* click the username field edit text and put in a random username */
+        onView(withId(R.id.editTextUsernameSignUp)).perform(click());
+        onView(withId(R.id.editTextUsernameSignUp)).perform(ViewActions.typeText("signuptest"));
+        /* click the password field edit text and put in a random password */
+        onView(withId(R.id.editTextPasswordSignUp)).perform(click());
+        onView(withId(R.id.editTextPasswordSignUp)).perform(ViewActions.typeText("signuptest"));
+        /* click the confirm password field edit text and put in a random password */
+        onView(withId(R.id.editTextConfirmPasswordSignUp)).perform(click());
+        onView(withId(R.id.editTextConfirmPasswordSignUp)).perform(ViewActions.typeText("signuptest"));
+        /* click the sign up button */
+        onView(withId(R.id.buttonSignUp)).perform(click());
+        Thread.sleep(3000);
+        /* click the logout popup button (would work if we were in main) */
+        onView(withId(R.id.btn_logout)).perform(click());
+        /* check that the username shown in the profile popup is our test username (shows successful sign up) */
+        onView(withId(R.id.profile_username)).check(matches(withText("signuptest's\tSweetHome")));
+        /* get the user so we can delete it now that the test is over */
+        FirebaseUser user = userAuth.getCurrentUser();
+        /* click logout */
+        onView(withId(R.id.profile_logout)).perform(click());
+        Thread.sleep(3000);
+        /* delete the user account */
+        user.delete();
+        /* delete the user username/email mapping in the users collection */
+        usersRef.whereEqualTo("email", "signuptest@signuptest.ca")
+                .limit(1)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot doc = task.getResult().getDocuments().get(0);
+                        if (doc.exists()) {
+                            String userDocID = doc.getId();
+                            usersRef.document(userDocID).delete();
+                        }
+                    }
+                });
     }
 
     @After
